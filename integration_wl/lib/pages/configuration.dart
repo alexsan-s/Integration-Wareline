72577dc2b4b7dfc95f2aaa5300f36c0a73e3c9b3
Alexsander da Silva <alexsans@outlook.com.br>
2020-07-14 17:47:04 -0300

    Connect to database

--- integration_wl/lib/pages/configuration.dart
+++ integration_wl/lib/pages/configuration.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:integration_wl/util/setting.dart';
 import 'package:preferences/preferences.dart';
 
 class Configuration extends StatefulWidget {
@@ -8,17 +8,8 @@ class Configuration extends StatefulWidget {
 }
 
 class _ConfigurationState extends State<Configuration> {
-  static String host = PrefService.getString('host');
-  static String port = PrefService.getString('port');
-  static String database = PrefService.getString('database');
-  static String username = PrefService.getString('username');
-  static String password = PrefService.getString('password');
-
-  var ted_host = TextEditingController(text: host);
-  var ted_port = TextEditingController(text: port);
-  var ted_database = TextEditingController(text: database);
-  var ted_username = TextEditingController(text: username);
-  var ted_password = TextEditingController(text: password);
+  var menssage = TextEditingController();
+  Setting setting = Setting();
 
   @override
   Widget build(BuildContext context) {
@@ -26,33 +17,34 @@ class _ConfigurationState extends State<Configuration> {
         appBar: AppBar(
           title: Text("Configuração do banco de dados"),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Text("Host"),
-            TextField(controller: ted_host),
-            Text("Port"),
-            TextField(controller: ted_port),
-            Text("BD"),
-            TextField(controller: ted_database),
-            Text("Username"),
-            TextField(controller: ted_username),
-            Text("Password"),
-            TextField(
-              controller: ted_password,
-              obscureText: true,
-            ),
-            FloatingActionButton(onPressed: connection())
-          ],
+        body: PreferencePage([
+          PreferenceTitle("host"),
+          TextFieldPreference('host', 'host'),
+          PreferenceTitle("Port"),
+          TextFieldPreference('port', 'port'),
+          PreferenceTitle("Database"),
+          TextFieldPreference('database', 'database'),
+          PreferenceTitle("User"),
+          TextFieldPreference('user', 'username'),
+          PreferenceTitle("Password"),
+          TextFieldPreference(
+            'password',
+            'password',
+            obscureText: true,
+          ),
+        ]),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            var temp = await setting.connection();
+            menssage = TextEditingController(text: '$temp');
+            print(temp);
+            setState(() {});
+          },
+          child: Icon(Icons.assignment_turned_in),
+        ),
+        bottomNavigationBar: TextField(
+          controller: menssage,
+          textAlign: TextAlign.center,
         ));
-  }
-
-  connection() {
-    print(ted_host);
-    PrefService.setString('host', ted_host.toString());
-    PrefService.setString('port', ted_port.toString());
-    PrefService.setString('database', ted_database.toString());
-    PrefService.setString('user', ted_username.toString());
-    PrefService.setString('password', ted_password.toString());
   }
 }
--- integration_wl/lib/util/setting.dart
+++ integration_wl/lib/util/setting.dart
@@ -1,1 +1,40 @@
-class Setting {}
+import 'package:postgres/postgres.dart';
+import 'package:preferences/preferences.dart';
+
+class Setting {
+  Future<String> connection() async {
+    var connection = await PostgreSQLConnection(
+        PrefService.getString('host'),
+        int.parse(PrefService.getString('port')),
+        PrefService.getString('database'),
+        username: PrefService.getString('username'),
+        password: PrefService.getString('password'));
+
+    try {
+      if (await connection.open() == null) return "Conectado com sucesso.";
+    } on PostgreSQLException {
+      return "Não foi possível conectar ao banco de dados.";
+    } catch (SocketException) {
+      return "Host/Port inacessível";
+    }
+  }
+  // banco() async {
+  //   // ignore: await_only_futures
+  //   // var connection = await PostgreSQLConnection(PrefService.getString('host'),
+  //   //     PrefService.getInt('port'), PrefService.getString('database'),
+  //   //     username: PrefService.getString('username'),
+  //   //     password: PrefService.getString('password'));
+  // //   await connection.open();
+
+  //   var underline = '';
+
+  //   for (var i = 0; i < 1351; i++) {
+  //     underline = underline + '_';
+  //   }
+  //   List<List<dynamic>> teste = await connection.query(
+  //       "select nomeope from cadope where podeusar like '${underline}1%' and nomeope like 'TESTE'");
+  //   for (final row in teste) {
+  //     print(row[0]);
+  //   }
+  // }
+}
