c3b4d8b14d484b26b50668ac232354a69a6cb2e2
Alexsander da Silva <alexsans@outlook.com.br>
2020-07-17 13:01:52 -0300

    login ok

--- integration_wl/lib/util/query.dart
+++ integration_wl/lib/util/query.dart
@@ -1,10 +1,9 @@
 class Query {
   String login(String operator, String password) {
-    return """
-    SELECT co2.codope 
-    FROM cadope2 cO2 
-    LEFT JOIN cadope co ON co2.codope = co.codope 
-    WHERE co.username LIKE '$operator' AND 
-    co2.senha = '$password' """;
+    return """SELECT co2.codope FROM cadope2 cO2 LEFT JOIN cadope co ON co2.codope = co.codope WHERE co.username LIKE '$operator' AND co2.senha = '$password' """;
+  }
+
+  String module(var underline, String codope) {
+    return """select nomeope from cadope where podeusar like '${underline}1%' and codope like '$codope'""";
   }
 }
--- integration_wl/lib/util/setting.dart
+++ integration_wl/lib/util/setting.dart
@@ -6,20 +6,21 @@ import 'package:postgres/postgres.dart';
 import 'package:preferences/preferences.dart';
 
 class Setting {
-  var conn;
+  var db;
   String underline = '';
   Query query = Query();
 
   Future database() async {
     try {
       // ignore: await_only_futures
-      final connection = await PostgreSQLConnection(
+      db = await PostgreSQLConnection(
           PrefService.getString('host'),
           int.parse(PrefService.getString('port')),
           PrefService.getString('database'),
           username: PrefService.getString('username'),
           password: PrefService.getString('password'));
-      return connection;
+      await db.open();
+      return db;
     } on PostgreSQLException {
       return "Não foi possível conectar ao banco de dados.";
     } catch (SocketException) {
@@ -28,23 +29,32 @@ class Setting {
   }
 
   connection() async {
-    conn = await database();
+    try {
+      var conn = await database();
+    } on PostgreSQLException {
+      print('sorry, too many clients already');
+    }
   }
 
   open(TextEditingController operator, TextEditingController password,
       int module) async {
-    connection();
+    await database();
     switch (module) {
       case 12:
         var pw = convertSha256(password.text);
         var result = query.login(operator.text, pw.toString());
         try {
-          List<List<dynamic>> row = await conn.query(result);
-          print('aaaaaa');
+          List<List<dynamic>> row = await db.query('$result');
+          var codope;
+          for (final row in row) {
+            codope = row[0];
+          }
+          login(codope, module);
         } on PostgreSQLException {
           print('Não foi conectado ao banco');
+        } on NoSuchMethodError {
+          print('NoSuchMethodError');
         }
-        login(module);
         break;
       default:
     }
@@ -63,22 +73,16 @@ class Setting {
     }
   }
 
-  login(int module) async {
-    connection();
+  login(var codope, int module) async {
+    // connection();
     underlineStr(module);
-    print(
-        "select nomeope from cadope where podeusar like '${underline}1%' and nomeope like 'TESTE'");
-    // List<List<dynamic>> teste = await conn.query(
-    //     "select nomeope from cadope where podeusar like '${underline}1%' and nomeope like 'TESTE'");
-    // for (final row in teste) {
-    //   print(row[0]);
-    // }
+    var result = query.module(underline, codope);
+    print(result);
+    List<List<dynamic>> row = await db.query('$result');
+    var nomeope;
+    for (final row in row) {
+      nomeope = row[0];
+    }
+    print(nomeope);
   }
 }
-
-//   List<List<dynamic>> teste = await connection.query(
-//       "select nomeope from cadope where podeusar like '${underline}1%' and nomeope like 'TESTE'");
-//   for (final row in teste) {
-//     print(row[0]);
-//   }
-// }
