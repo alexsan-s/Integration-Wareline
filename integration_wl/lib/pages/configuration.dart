1f53d6d5db65ba4037814db46ed217eca77e42a6
alexsan-s <64074234+alexsan-s@users.noreply.github.com>
2020-07-19 00:43:12 -0300

    Show Alert Dialog

--- integration_wl/lib/pages/configuration.dart
+++ integration_wl/lib/pages/configuration.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:integration_wl/util/setting.dart';
 import 'package:preferences/preferences.dart';
@@ -8,43 +9,50 @@ class Configuration extends StatefulWidget {
 }
 
 class _ConfigurationState extends State<Configuration> {
-  var menssage = TextEditingController();
   Setting setting = Setting();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Configuração do banco de dados"),
+      appBar: AppBar(
+        title: Text("Configuração do banco de dados"),
+      ),
+      body: PreferencePage([
+        PreferenceTitle("host"),
+        TextFieldPreference('host', 'host'),
+        PreferenceTitle("Port"),
+        TextFieldPreference('port', 'port'),
+        PreferenceTitle("Database"),
+        TextFieldPreference('database', 'database'),
+        PreferenceTitle("User"),
+        TextFieldPreference('user', 'username'),
+        PreferenceTitle("Password"),
+        TextFieldPreference(
+          'password',
+          'password',
+          obscureText: true,
         ),
-        body: PreferencePage([
-          PreferenceTitle("host"),
-          TextFieldPreference('host', 'host'),
-          PreferenceTitle("Port"),
-          TextFieldPreference('port', 'port'),
-          PreferenceTitle("Database"),
-          TextFieldPreference('database', 'database'),
-          PreferenceTitle("User"),
-          TextFieldPreference('user', 'username'),
-          PreferenceTitle("Password"),
-          TextFieldPreference(
-            'password',
-            'password',
-            obscureText: true,
-          ),
-        ]),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            var temp = await setting.connection();
-            menssage = TextEditingController(text: '$temp');
-            print(temp);
-            setState(() {});
-          },
-          child: Icon(Icons.assignment_turned_in),
-        ),
-        bottomNavigationBar: TextField(
-          controller: menssage,
-          textAlign: TextAlign.center,
-        ));
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          var temp = await setting.connection();
+          showDialog(
+              context: context,
+              child: AlertDialog(
+                title: Text(
+                  "Conexão com o banco",
+                  textAlign: TextAlign.center,
+                ),
+                content: Text(
+                  temp.toString(),
+                  textAlign: TextAlign.center,
+                ),
+              ));
+          print(temp);
+          setState(() {});
+        },
+        child: Icon(Icons.assignment_turned_in),
+      ),
+    );
   }
 }
